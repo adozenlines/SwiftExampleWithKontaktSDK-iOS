e03af14b18c2d0dc9b00880de22309f143abc731
Luke <lukasz.hlebowicz@gmail.com>
2014-11-14 22:20:28 +0100

    Initial Commit

---
+++ SwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftExample
+//
+//  Created by Lukasz Hlebowicz on 14/11/14.
+//  Copyright (c) 2014 Lukasz Hlebowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
